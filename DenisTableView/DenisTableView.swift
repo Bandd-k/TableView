37cbc3e80465b615f1142a8af446a61f79123683
denisKa <denis.karpenko@numbrs.com>
2019-05-13 15:39:59 +0200

    tableView

---
+++ DenisTableView/DenisTableView.swift
@@ -0,0 +1,9 @@
+//
+//  DenisTableView.swift
+//  DenisTableView
+//
+//  Created by Denis Karpenko on 2019-05-13.
+//  Copyright © 2019 Denis Karpenko. All rights reserved.
+//
+
+import Foundation
