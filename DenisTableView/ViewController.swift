09820feb9e34e29a8f1ae31e66a00542881a06b9
denisKa <denis.karpenko@numbrs.com>
2019-05-13 15:23:57 +0200

    Initial Commit

---
+++ DenisTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DenisTableView
+//
+//  Created by Denis Karpenko on 2019-05-13.
+//  Copyright © 2019 Denis Karpenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
